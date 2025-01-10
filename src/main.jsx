7d8d49fed943abc74b852b50c55fee76c21e641b
mofacchirul <mofaccharulhoque@gmail.com>
2025-01-10 11:37:14 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
         element:<PriviteRouter>
           <View_Details></View_Details>
         </PriviteRouter>,
-        loader:({params})=>fetch(`https://assignment-11-server-side-mu-ten.vercel.app/volunteer/${params.id}`),    
+        loader:({params})=>fetch(`https://assignment-11-server-side-mu-ten.vercel.app/Volunteers/${params.id}`),    
       },
       {
         path:'/showall',
